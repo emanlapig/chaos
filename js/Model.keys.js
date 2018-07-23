660073d537d1fb1d869763b6c44d4aba6c5d49e7
emanlapig <emmanlapig@gmail.com>
2018-07-22 23:56:33 -0400

    put keys in their own file

---
+++ js/Model.keys.js
@@ -0,0 +1,688 @@
+// enochian keys
+var enochian_keys = [
+	{
+		title: "The First Key",
+		content: [
+			{
+				eno: "Ol",
+				eng: "I"
+			},{
+				eno: "sonf",
+				eng: "reign"
+			},{
+				eno: "vors-g,",
+				eng: "over you,"
+			},{
+				eno: "goho",
+				eng: "saith"
+			},{
+				eno: "Iad",
+				eng: "the God"
+			},{
+				eno: "Balt,",
+				eng: "of Justice,"
+			},{
+				eno: "lansh",
+				eng: "in power exalted"
+			},{
+				eno: "calz",
+				eng: "over the firmaments"
+			},{
+				eno: "vonpho;",
+				eng: "of wrath;"
+			},{
+				eno: "sobra",
+				eng: "in whose"
+			},{
+				eno: "z-ol",
+				eng: "hands"
+			},{
+				eno: "Ror",
+				eng: "the Sun"
+			},{
+				eno: "I",
+				eng: "is"
+			},{
+				eno: "ta",
+				eng: "as"
+			},{
+				eno: "nazpsad,",
+				eng: "a sword,"
+			},{
+				eno: "Graa",
+				eng: "and the Moon"
+			},{
+				eno: "ta",
+				eng: "as"
+			},{
+				eno: "malprg,",
+				eng: "a through-thrusting fire,"
+			},{
+				eno: "ds",
+				eng: "which"
+			},{
+				eno: "holq",
+				eng: "measureth"
+			},{
+				eno: "qaa",
+				eng: "your garments"
+			},{
+				eno: "nothoa",
+				eng: "in the midst"
+			},{
+				eno: "zimz",
+				eng: "of my vestitures,"
+			},{
+				eno: "od",
+				eng: "and"
+			},{
+				eno: "commah",
+				eng: "trussed you together"
+			},{
+				eno: "ta",
+				eng: "as"
+			},{
+				eno: "nobloh",
+				eng: "the palms"
+			},{
+				eno: "zien;",
+				eng: "of my hands;"
+			},{
+				eno: "soba",
+				eng: "whose"
+			},{
+				eno: "thil",
+				eng: "seats"
+			},{
+				eno: "gnonp",
+				eng: "I have garnished"
+			},{
+				eno: "prge",
+				eng: "with the fire"
+			},{
+				eno: "aldi,",
+				eng: "of gathering,"
+			},{
+				eno: "ds",
+				eng: "which"
+			},{
+				eno: "urbs",
+				eng: "beautified"
+			},{
+				eno: "oboleh",
+				eng: "your garments"
+			},{
+				eno: "g-rsam;",
+				eng: "with admiration;"
+			},{
+				eno: "casarm",
+				eng: "to whom"
+			},{
+				eno: "ohorela",
+				eng: "I made a Law"
+			},{
+				eno: "caba",
+				eng: "to govern"
+			},{
+				eno: "pir",
+				eng: "the Holy Ones,"
+			},{
+				eno: "ds",
+				eng: "which"
+			},{
+				eno: "zonrensg",
+				eng: "delivered you"
+			},{
+				eno: "cab",
+				eng: "a rod"
+			},{
+				eno: "erm",
+				eng: "with the ark"
+			},{
+				eno: "Jadnah.",
+				eng: "of knowledge."
+			},{
+				eno: "Pilah,",
+				eng: "Moreover,"
+			},{
+				eno: "farzm",
+				eng: "you filled up your voices"
+			},{
+				eno: "znrza",
+				eng: "and swore"
+			},{
+				eno: "adna",
+				eng: "obedience"
+			},{
+				eno: "gono",
+				eng: "and faith"
+			},{
+				eno: "Iadpil",
+				eng: "to Him"
+			},{
+				eno: "ds",
+				eng: "that"
+			},{
+				eno: "hom",
+				eng: "liveth"
+			},{
+				eno: "toh;",
+				eng: "and triumpheth;"
+			},{
+				eno: "soba",
+				eng: "whose"
+			},{
+				eno: "ipam",
+				eng: "(beginning) is not"
+			},{
+				eno: "lu",
+				eng: "nor end"
+			},{
+				eno: "ipamis;",
+				eng: "cannot be;"
+			},{
+				eno: "ds",
+				eng: "which"
+			},{
+				eno: "loholo",
+				eng: "shineth"
+			},{
+				eno: "vep",
+				eng: "as a flame"
+			},{
+				eno: "zomd",
+				eng: "in the midst"
+			},{
+				eno: "poamal,",
+				eng: "of your palace,"
+			},{
+				eno: "od",
+				eng: "and"
+			},{
+				eno: "bogpa",
+				eng: "reigneth"
+			},{
+				eno: "aai",
+				eng: "amongst you"
+			},{
+				eno: "ta",
+				eng: "as"
+			},{
+				eno: "piap",
+				eng: "the balance"
+			},{
+				eno: "piamol",
+				eng: "of righteousness"
+			},{
+				eno: "od",
+				eng: "and"
+			},{
+				eno: "vaoan.",
+				eng: "truth."
+			},{
+				eno: "ZACARe",
+				eng: "Move,"
+			},{
+				eno: "ca,",
+				eng: "therefore,"
+			},{
+				eno: "od",
+				eng: "and"
+			},{
+				eno: "ZAMRAN!",
+				eng: "show yourselves!"
+			},{
+				eno: "Odo",
+				eng: "Open"
+			},{
+				eno: "cicle",
+				eng: "the mysteries"
+			},{
+				eno: "qaa.",
+				eng: "of your creation."
+			},{
+				eno: "Zorge,",
+				eng: "Be friendly unto me:"
+			},{
+				eno: "lap",
+				eng: "for"
+			},{
+				eno: "zirdo",
+				eng: "I am"
+			},{
+				eno: "noco",
+				eng: "the servant"
+			},{
+				eno: "MAD,",
+				eng: "of the same your God,"
+			},{
+				eno: "hoath",
+				eng: "the true worshipper"
+			},{
+				eno: "Jaida.",
+				eng: "of the Highest."
+			},
+		]
+	},{
+		title: "The Second Key",
+		content:[
+			{
+				eno: "Adgt",
+				eng: "Can"
+			},{
+				eno: "Vpaah",
+				eng: "the Wings"
+			},{
+				eno: "Zongom",
+				eng: "of the Winds"
+			},{
+				eno: "faaip",
+				eng: "understand"
+			},{
+				eno: "sald,",
+				eng: "your voices of wonder,"
+			},{
+				eno: "viiv",
+				eng: "O you, the Second"
+			},{
+				eno: "L?",
+				eng: "of the First?"
+			},{
+				eno: "Sobam",
+				eng: "Whom"
+			},{
+				eno: "Ialprg",
+				eng: "the burning flames"
+			},{
+				eno: "izazaz",
+				eng: "have framed"
+			},{
+				eno: "piadph;",
+				eng: "within the depths of my jaws;"
+			},{
+				eno: "casarma",
+				eng: "whom"
+			},{
+				eno: "abramg",
+				eng: "I have prepared"
+			},{
+				eno: "ta",
+				eng: "as"
+			},{
+				eno: "talho",
+				eng: "cups"
+			},{
+				eno: "paracleda",
+				eng: "for a wedding,"
+			},{
+				eno: "q-ta",
+				eng: "or as"
+			},{
+				eno: "lorslq",
+				eng: "the flowers"
+			},{
+				eno: "turbs",
+				eng: "in their beauty"
+			},{
+				eno: "ooge",
+				eng: "for the chamber"
+			},{
+				eno: "Baltoh.",
+				eng: "of righteousness."
+			},{
+				eno: "Giui",
+				eng: "Stronger"
+			},{
+				eno: "chis",
+				eng: "are"
+			},{
+				eno: "lusd",
+				eng: "your feet"
+			},{
+				eno: "orri,",
+				eng: "than the barren stone,"
+			},{
+				eno: "od",
+				eng: "and"
+			},{
+				eno: "micalp",
+				eng: "mightier"
+			},{
+				eno: "chis",
+				eng: "are"
+			},{
+				eno: "bia",
+				eng: "your voices"
+			},{
+				eno: "ozongon.",
+				eng: "than the manifold winds."
+			},{
+				eno: "Lap",
+				eng: "For"
+			},{
+				eno: "noan",
+				eng: "you are become"
+			},{
+				eno: "trof",
+				eng: "a building"
+			},{
+				eno: "cors",
+				eng: "such"
+			},{
+				eno: "tage,",
+				eng: "as is not,"
+			},{
+				eno: "oq",
+				eng: "but"
+			},{
+				eno: "manin",
+				eng: "in the mind"
+			},{
+				eno: "Iaidon.",
+				eng: "of the All-powerful."
+			},{
+				eno: "Torzu,",
+				eng: "Arise,"
+			},{
+				eno: "gohe-l!",
+				eng: "saith the First!"
+			},{
+				eno: "ZACAR,",
+				eng: "Move,"
+			},{
+				eno: "ca,",
+				eng: "therefore,"
+			},{
+				eno: "c-noqod.",
+				eng: "unto his servants!"
+			},{
+				eno: "ZAMRAN",
+				eng: "Show yourselves"
+			},{
+				eno: "micalzo,",
+				eng: "in power,"
+			},{
+				eno: "od",
+				eng: "and"
+			},{
+				eno: "ozazm",
+				eng: "make me"
+			},{
+				eno: "urelp,",
+				eng: "a strong see-thing,"
+			},{
+				eno: "lap",
+				eng: "for"
+			},{
+				eno: "zir",
+				eng: "I am"
+			},{
+				eno: "Ioiad",
+				eng: "of Him that liveth forever"
+			},
+		]
+	},{
+		title: "The Third Key",
+		content:[
+			{
+				eno: "Micma,",
+				eng: "Behold,"
+			},{
+				eno: "goho",
+				eng: "saith"
+			},{
+				eno: "Piad,",
+				eng: "your God,"
+			},{
+				eno: "zir",
+				eng: "I am"
+			},{
+				eno: "comselh",
+				eng: "a circle"
+			},{
+				eno: "a-zien",
+				eng: "on whose hands"
+			},{
+				eno: "biab",
+				eng: "stand"
+			},{
+				eno: "os",
+				eng: "12"
+			},{
+				eno: "londoh;",
+				eng: "kingdoms;"
+			},{
+				eno: "norz",
+				eng: "six"
+			},{
+				eno: "chis",
+				eng: "are"
+			},{
+				eno: "othil",
+				eng: "the seats"
+			},{
+				eno: "gigipah,",
+				eng: "of living breath,"
+			},{
+				eno: "vndl",
+				eng: "the rest"
+			},{
+				eno: "chis",
+				eng: "are"
+			},{
+				eno: "ta",
+				eng: "as"
+			},{
+				eno: "puim,",
+				eng: "sharp sickles,"
+			},{
+				eno: "q",
+				eng: "or"
+			},{
+				eno: "mospleh",
+				eng: "the horns"
+			},{
+				eno: "teloch;",
+				eng: "of death;"
+			},{
+				eno: "quiin",
+				eng: "wherein"
+			},{
+				eno: "toltorg",
+				eng: "the creatures of the Earth"
+			},{
+				eno: "chis",
+				eng: "are"
+			},{
+				eno: "i-chis-ge",
+				eng: "and are not"
+			},{
+				eno: "m",
+				eng: "except"
+			},{
+				eno: "ozien;",
+				eng: "by mine own hand;"
+			},{
+				eno: "ds-t",
+				eng: "which also"
+			},{
+				eno: "brgda",
+				eng: "sleep"
+			},{
+				eno: "od",
+				eng: "and"
+			},{
+				eno: "torzul.",
+				eng: "shall rise."
+			},{
+				eno: "I-li",
+				eng: "In the first"
+			},{
+				eno: "eol",
+				eng: "I made you"
+			},{
+				eno: "balzarg",
+				eng: "stewards"
+			},{
+				eno: "od",
+				eng: "and"
+			},{
+				eno: "aala",
+				eng: "placed you"
+			},{
+				eno: "thiln",
+				eng: "in seats"
+			},{
+				eno: "os",
+				eng: "12"
+			},{
+				eno: "netaab,",
+				eng: "of government,"
+			},{
+				eno: "dluga",
+				eng: "giving"
+			},{
+				eno: "vomsarg",
+				eng: "unto every one of you"
+			},{
+				eno: "lonsa",
+				eng: "power"
+			},{
+				eno: "capmiali",
+				eng: "successively"
+			},{
+				eno: "vors",
+				eng: "over"
+			},{
+				eno: "cla,",
+				eng: "456,"
+			},{
+				eno: "homil",
+				eng: "the true ages"
+			},{
+				eno: "cocasb,",
+				eng: "of time,"
+			},{
+				eno: "fafen,",
+				eng: "to the intent that,"
+			},{
+				eno: "izizop",
+				eng: "from the highest vessels"
+			},{
+				eno: "od",
+				eng: "and"
+			},{
+				eno: "miinoag",
+				eng: "the corners"
+			},{
+				eno: "de",
+				eng: "of"
+			},{
+				eno: "g-netaab,",
+				eng: "your governments,"
+			},{
+				eno: "vaun",
+				eng: "you might work"
+			},{
+				eno: "nanaeel,",
+				eng: "my power,"
+			},{
+				eno: "panpir",
+				eng: "pouring down"
+			},{
+				eno: "malpirgi",
+				eng: "the fires of life and increase"
+			},{
+				eno: "Caosg",
+				eng: "on the Earth"
+			},{
+				eno: "pild.",
+				eng: "continually."
+			},{
+				eno: "Noan",
+				eng: "Thus you are become"
+			},{
+				eno: "unalah",
+				eng: "the skirts"
+			},{
+				eno: "Balt",
+				eng: "of Justice"
+			},{
+				eno: "od",
+				eng: "and"
+			},{
+				eno: "Vooan.",
+				eng: "Truth."
+			},{
+				eno: "Dooiap",
+				eng: "In the name of the same,"
+			},{
+				eno: "MAD,",
+				eng: "your God,"
+			},{
+				eno: "goholor,",
+				eng: "lift up,"
+			},{
+				eno: "gohus,",
+				eng: "I say,"
+			},{
+				eno: "amiran.",
+				eng: "yourselves."
+			},{
+				eno: "Micma,",
+				eng: "Behold,"
+			},{
+				eno: "iehusoz",
+				eng: "his mercies"
+			},{
+				eno: "cacacom,",
+				eng: "flourish,"
+			},{
+				eno: "od",
+				eng: "and"
+			},{
+				eno: "dooain",
+				eng: "name"
+			},{
+				eno: "noar",
+				eng: "is become"
+			},{
+				eno: "micaolz",
+				eng: "mighty"
+			},{
+				eno: "aaiom;",
+				eng: "amongst us;"
+			},{
+				eno: "casarmg",
+				eng: "in whom"
+			},{
+				eno: "gohia,",
+				eng: "we say,"
+			},{
+				eno: "ZACAR,",
+				eng: "move,"
+			},{
+				eno: "uniglag",
+				eng: "descend"
+			},{
+				eno: "od",
+				eng: "and"
+			},{
+				eno: "imvamar,",
+				eng: "apply yourselves unto us,"
+			},{
+				eno: "pugo",
+				eng: "as unto"
+			},{
+				eno: "plapli",
+				eng: "partakers"
+			},{
+				eno: "ananael",
+				eng: "of the secret wisdom"
+			},{
+				eno: "qaan.",
+				eng: "of your creation."
+			},
+		]
+	}
+];
